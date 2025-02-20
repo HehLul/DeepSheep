8e6898ed47d5186fa868c1c16b9d3320b40552ff
Ahmed Salman <81371023+HehLul@users.noreply.github.com>
2025-02-20 16:59:30 -0500

    UPDATED email input color.

--- app/src/app/launch/page.js
+++ app/src/app/launch/page.js
@@ -155,7 +155,7 @@ export default function LaunchPage() {
                       Your Email
                     </label>
                     <div className="relative">
-                      <Mail className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" />
+                      <Mail className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-900" />
                       <input
                         type="email"
                         required
